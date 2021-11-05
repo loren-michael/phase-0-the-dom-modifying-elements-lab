99d7ea8982b22f24e4f2f4f1834dc6b471d48275
loren <lorenmichaelh@gmail.com>
2021-11-05 15:29:15 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.body.removeChild(main);
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Loren is the champion.";
+document.body.append(newHeader);
